0dbfeb484e911d4ca40ac26991d11a0c834aa562
Bhavyai Gupta <zbhavyai@gmail.com>
2026-02-16 15:09:51 -0700

    update hook to use mapfile along with git diff filter

--- .hooks/pre-commit.sh
+++ .hooks/pre-commit.sh
@@ -1,23 +1,27 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
-block() {
+function block() {
     echo -e "\n\n"
     echo "$@"
     echo "[ERROR] Commit blocked."
     exit 1
 }
 
-lint_checks() {
-    local staged
-    staged=$(git diff --name-only --cached --exit-code -- '*.java')
-    ret=$?
-    if [ $ret -eq 0 ]; then
+CHECKS="format_checks"
+
+function format_checks() {
+    mapfile -d '' -t staged_java < <(git diff --cached --name-only -z --diff-filter=ACMR -- '*.java' || true)
+
+    if ((${#staged_java[@]} == 0)); then
         return 0
     fi
 
-    (./mvnw --quiet --batch-mode spotless:check) || block "[ERROR] Lint checks failed; run 'make format' to fix."
+    if ! ./mvnw --quiet --batch-mode spotless:check; then
+        block "[ERROR] Formatting checks failed; run 'make format' to fix."
+    fi
 }
 
-(lint_checks) || exit $?
+for CHECK in $CHECKS; do
+    ($CHECK) || exit $?
+done
