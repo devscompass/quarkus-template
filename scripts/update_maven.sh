82d2736acce2c9f206084b7dc71ef44e1fb97774
Bhavyai Gupta <zbhavyai@gmail.com>
2026-02-16 22:11:38 -0700

    re-format update maven script

--- scripts/update_maven.sh
+++ scripts/update_maven.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
-
 set -euo pipefail
 
 # set new version of maven
 if [[ $# -ge 1 ]]; then
-  mavenVersion="$1"
-  echo "[ INFO] Using version from command line: ${mavenVersion}"
+    mavenVersion="$1"
+    echo "[ INFO] Using version from command line: ${mavenVersion}"
 else
-  read -rp "[ INFO] Enter Apache Maven version (e.g. 3.9.11): " mavenVersion
+    read -rp "[ INFO] Enter Apache Maven version (e.g. 3.9.11): " mavenVersion
 fi
 echo
 
 if [[ -z "${mavenVersion}" ]]; then
-  echo "[ERROR] No version provided. Exiting."
-  exit 1
+    echo "[ERROR] No version provided. Exiting."
+    exit 1
 fi
 
 # update version
